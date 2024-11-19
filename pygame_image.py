b843c6a4458b4ec40bed73c368397e86948016cb
SMS <c0a230690f@edu.teu.ac.jp>
2024-11-19 17:05:25 +0900

    練習８:こうかとん移動式完了

--- pygame_image.py
+++ pygame_image.py
@@ -9,16 +9,35 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png") 
-    kk_img = pg.transform.flip(kk_img, True,False)
+    bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像をsurfaceを作成する
+    bg_img2= pg.transform.flip (bg_img ,True,False)
+    kk_img = pg.image.load("fig/3.png") #こうかとん画像surafaceを作成する
+    kk_img = pg.transform.flip(kk_img, True,False) #こうかとんを左右反転
+    kk_rct = kk_img.get_rect() #こうかとんrectを摂取する
+    kk_rct.center = 300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -tmr%800
-        screen.blit(bg_img, [-x, 0]) #screen Surfaceに背景画像surfaceを貼り付ける
-        screen.blit(bg_img, [300, 200])  #screen sufaceにこうかとん画像
+        key_lst = pg.key.get_pressed()
+
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,+1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1,0)
+        
+        x = -(tmr%3200) #練習6-2 
+        screen.blit(bg_img, [x, 0]) #screen Surfaceに背景画像surfaceを貼り付ける
+        screen.blit(bg_img2,[x+1600,0])
+        screen.blit(bg_img,[x+3200,0])
+        screen.blit(bg_img,[x+4800,0])
+        #screen.blit(kk_img, [300, 200])  #screen sufaceにこうかとん画像
+        screen.blit(kk_img,kk_rct)
+
         pg.display.update()
         tmr += 1        
         clock.tick(200)
